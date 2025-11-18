a3f7bfd1536c450e6f37ce79bc45798c47c14257
Dominik Mikolajczyk <domin.mikol@gmail.com>
2025-11-18 19:56:03 +0100

    new autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,3 +5,16 @@ vim.api.nvim_create_autocmd('FileType', {
         require("jdtls.jdtls_setup").setup()
     end
 })
+
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
